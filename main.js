d2e743aedeafc78943b35c00ee69df96643577b2
Luis Leyva <lmorales6@ucol.mx>
2020-11-29 00:40:48 -0600

    faltan algunos aspectos

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ var Duracion = document.getElementById('etDuracionB');
 var SiguienteB = document.getElementById('etSiguienteB');
 var AnteriorB = document.getElementById('etAnteriorB');
 
-var horaInicial = document.getElementById('horaI');
-var horaFinal = document.getElementById('horaF');
+var horarioInicial = document.getElementById('horaI');
+var horarioFinal = document.getElementById('horaF');
 var baseInicio = document.getElementById('baseInicio');
 
 var ruta = new Ruta;
@@ -28,5 +28,54 @@ Agregar.addEventListener('click' , () => {
     let nombre = BaseN.value;
     let minutos = Number(Minutos.value);
     let base  = new Base (nombre, minutos);
-    ruta.Agregar(base)
+    ruta.agregarBase(base);
+    console.log(base);
+})
+
+Eliminar,addEventListener('click' , () => {
+    let nombre = BaseN.value;
+    ruta.eliminarBase(nombre);
+    console.log(ruta);
+})
+
+Buscar.addEventListener('click' , () => {
+    let nombre = BaseN.value;
+    let base = ruta.buscarBase(nombre);
+    NombreB.innerHTML = 'Nombre: ' + base.nombre;
+    Duracion.innerHTML = 'Duracion: ' + base.minutos;
+    SiguienteB.innerHTML = 'Siguiente: ' + base.siguiente.nombre;
+    AnteriorB.innerHTML = 'Anterior: ' + base.anterior.nombre;
+
+    console.log('Nombre: ' + base.nombre);
+    console.log('Duracion: ' + base.minutos);
+    console.log('Siguiente: ' + base.siguiente.nombre);
+    console.log('Anterior: ' + base.anterior.nombre);
+})
+
+Imprimir.addEventListener('click', () => {
+    Lista.innerHTML = 'Bases: ' + ruta.listarBases;
+    console.log(ruta.listarBases());
+})
+
+Insertar.addEventListener('click', () => {
+    let nombre = BaseN.value;
+    let minutos = Number(Minutos.value);
+    let posicion = Number(Posicion.value);
+    let base = new Base (nombre, minutos);
+    ruta.insertarBase(base, posicion);
+    console.log(ruta);
+})
+
+CrearTarjeta.addEventListener('click', () => {
+    let horaInicial = new Date();
+    horaInicial.setHours(horarioInicial.value.slice(0,2));
+    horaInicial.setMinutes(horarioInicial.value.slice(-2));
+
+    let horaFinal = new Date();
+    horaFinal.setHours(horarioFinal.value.slice(0,2));
+    horaFinal.setMinutes(horarioFinal.value.slice(-2));
+
+    let nombre = baseInicio.value;
+
+    console.log(ruta.crearRecorrido(nombre, horaInicial, horaFinal));
 })
